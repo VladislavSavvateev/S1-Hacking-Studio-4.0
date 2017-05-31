b94e1292263101281dd3d619fa53bf99877ff9af
VladislavSavvateev <savvateevvlad@mail.ru>
2017-06-01 02:12:38 +0900

    Поправлен интерфейс

--- S1 Hacking Studio 4.0/BossGenerator.Designer.cs
+++ S1 Hacking Studio 4.0/BossGenerator.Designer.cs
@@ -23,9 +23,11 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BossGenerator));
 			this.label1 = new System.Windows.Forms.Label();
 			this.rchCode = new System.Windows.Forms.RichTextBox();
 			this.btnGenerate = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -33,16 +35,15 @@
 			this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.label1.Location = new System.Drawing.Point(12, 9);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(392, 44);
+			this.label1.Size = new System.Drawing.Size(392, 70);
 			this.label1.TabIndex = 0;
-			this.label1.Text = "Работа генератора предельно проста! Просто напишите в свободной форме, какого бос" +
-    "са вы хотите создать и магическим путём программа сгенерирует код!";
+			this.label1.Text = resources.GetString("label1.Text");
 			// 
 			// rchCode
 			// 
-			this.rchCode.Location = new System.Drawing.Point(12, 56);
+			this.rchCode.Location = new System.Drawing.Point(12, 82);
 			this.rchCode.Name = "rchCode";
-			this.rchCode.Size = new System.Drawing.Size(392, 333);
+			this.rchCode.Size = new System.Drawing.Size(392, 307);
 			this.rchCode.TabIndex = 1;
 			this.rchCode.Text = "";
 			this.rchCode.TextChanged += new System.EventHandler(this.rchCode_TextChanged);
@@ -58,11 +59,22 @@
 			this.btnGenerate.UseVisualStyleBackColor = true;
 			this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
 			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+			this.label2.Location = new System.Drawing.Point(312, 400);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(92, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Powered by Gena";
+			// 
 			// BossGenerator
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(416, 430);
+			this.Controls.Add(this.label2);
 			this.Controls.Add(this.btnGenerate);
 			this.Controls.Add(this.rchCode);
 			this.Controls.Add(this.label1);
@@ -71,6 +83,7 @@
 			this.Name = "BossGenerator";
 			this.Text = "BossGenerator";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -79,5 +92,6 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.RichTextBox rchCode;
 		private System.Windows.Forms.Button btnGenerate;
+		private System.Windows.Forms.Label label2;
 	}
 }
